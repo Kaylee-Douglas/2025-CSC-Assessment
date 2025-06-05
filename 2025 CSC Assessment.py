a8f0ec6484679ff8c5148318e26e68916f932b06
Kaylee-Douglas <22081@nayland.school.nz>
2025-06-05 21:00:53 +1200

    add dictionaries for areas

--- 2025 CSC Assessment.py
+++ 2025 CSC Assessment.py
@@ -13,11 +13,24 @@ win = pygame.display.set_mode((480, 360))
 pygame.display.set_caption('Korero : Play')
 running = True
 
+# set up areas
+# each dictionary contains information for each area, including what is in each direction
+# '0' means that there is no room accessable from that point in that direction.
+home = {'no':"1", "description":"Starting point for user", "north":"0", "south":"2", "east":"0", "west":"0"}
+bus_stop = {'no':"2", "description":"A bus stop, which doesn't appear to be in use", "north":"1", "south":"0", "east":"3", "west":"0"}
+sign_posts = {'no':"3", "description":"Can be inspected for directions", "north":"0", "south":"4", "east":"0", "west":"2"}
+empty_road = {'no':"4", "description":"A road which seems to stretch forever", "north":"3", "south":"6", "east":"0", "west":"0"}
+translators_home = {'no':"5", "descriptions":"The man who lives here can translate between English and Maori", "north":"0", "south":"8", "east":"6", "west":"0"}
+market = {'no':"6", "description":"Food market, navigation point", "north":"4", "south":"9", "east":"7", "west":"5"}
+shop = {'no':"7", "description":"Where goods can be purchased", "north":"0", "south":"0", "east":"0", "west":"6"}
+native_bush = {'no':"8", "description":"Untamed native bush past the limits of town", "north":"5", "south":"0", "east":"9", "west":"0"}
+koro_house = {'no':"9", "description":"Home to the player character's Grandfather", "North":"6", "South":"0", "East":"0", "West":"8"}
+
 #while program is running
 while running:
     # check game is running
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # if exit button is pressed
+        if event.type == pygame.QUIT: # if it is identified that pygame has been quit
             running = False # stop & close program
    
     #Debug
