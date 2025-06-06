cbf0a94ce3f1060d68fcd4c70ee90b4421259552
Kaylee-Douglas <22081@nayland.school.nz>
2025-06-06 12:19:02 +1200

    correct dictionary data types and add active boolean value

--- 2025 CSC Assessment.py
+++ 2025 CSC Assessment.py
@@ -15,15 +15,15 @@ running = True
 # set up areas
 # each dictionary contains information for each area, including what is in each direction
 # '0' means that there is no room accessable from that point in that direction.
-area_1 = {'title':"home", "description":"Starting point for user", "north":"0", "south":"2", "east":"0", "west":"0"}
-area_2 = {'title':"bus stop", "description":"A bus stop, which doesn't appear to be in use", "north":"1", "south":"0", "east":"3", "west":"0"}
-area_3 = {'title':"sign post", "description":"Can be inspected for directions", "north":"0", "south":"4", "east":"0", "west":"2"}
-area_4 = {'title':"empty road", "description":"A road which seems to stretch forever", "north":"3", "south":"6", "east":"0", "west":"0"}
-area_5 = {'title':"translators home", "descriptions":"The man who lives here can translate between English and Maori", "north":"0", "south":"8", "east":"6", "west":"0"}
-area_6 = {'title':"market", "description":"Food market, navigation point", "north":"4", "south":"9", "east":"7", "west":"5"}
-area_7 = {'title':"shop", "description":"Where goods can be purchased", "north":"0", "south":"0", "east":"0", "west":"6"}
-area_8 = {'title':"native bush", "description":"Untamed native bush past the limits of town", "north":"5", "south":"0", "east":"9", "west":"0"}
-area_9= {'title':"koro's house", "description":"Home to the player character's Grandfather", "North":"6", "South":"0", "East":"0", "West":"8"}
+area_1 = {'title':"home", "description":"Starting point for user", "north": 0, "south": 2, "east": 0, "west": 0, "active": False}
+area_2 = {'title':"bus stop", "description":"A bus stop, which doesn't appear to be in use", "north": 1, "south": 0, "east": 3, "west": 0, "active": False}
+area_3 = {'title':"sign post", "description":"Can be inspected for directions", "north": 0, "south": 4, "east": 0, "west": 2, "active": False}
+area_4 = {'title':"empty road", "description":"A road which seems to stretch forever", "north": 3, "south": 6, "east": 0, "west": 0, "active": False}
+area_5 = {'title':"translators home", "descriptions":"The man who lives here can translate between English and Maori", "north": 0, "south": 8, "east": 6, "west": 0, "active": False}
+area_6 = {'title':"market", "description":"Food market, navigation point", "north": 4, "south": 9, "east": 7, "west": 5, "active": False}
+area_7 = {'title':"shop", "description":"Where goods can be purchased", "north": 0, "south": 0, "east": 0, "west": 0, "active": False}
+area_8 = {'title':"native bush", "description":"Untamed native bush past the limits of town", "north": 5, "south": 0, "east": 9, "west": 0, "active": False}
+area_9= {'title':"koro's house", "description":"Home to the player character's Grandfather", "North": 6, "South": 0, "East": 0, "West": 8, "active": False}
 locations_list = [area_1, area_2, area_3, area_4, area_5, area_6, area_7, area_8, area_9]
 
 # retreives sprites from the sprites folder
@@ -37,10 +37,10 @@ while running:
         if event.type == pygame.QUIT: # if it is identified that pygame has been quit
             running = False # stop & close program
     
-    def function_home():
+    def home_funct():
         win.blit(BCG_HOME, (0, 0)) # place background
 
-    def function_game():
+    def game_funct():
         win.blit(BCG_HOME, (0, 0)) # place background
         #if () == active:
     # updates screen
